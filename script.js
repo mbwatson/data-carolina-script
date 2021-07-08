e6618b58ad2741f3990908393277af6a897f098b
abunts <abunta@me.com>
2021-07-08 10:00:17 -0400

    format updates

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //fetch heel life events
 const fetchHLEvents = async () => {
-let events = []
-//get event info 
-return events;
+    let events = []
+    //get event info 
+    return events;
 }
 //fetch odum events
 const fetchOdumEvents = async () => {
     let events = []
     //get event info
-    return events; 
+    return events;
 }
 //create standard event object t
 const createEvent = (title, date, description, url) => {
-    return {title, date, description, url}
+    return { title, date, description, url }
 }
 //map events 
-const actualEvents = responses.map(createEvent => ({title, date, description, url}));
+const actualEvents = responses.map(createEvent => ({ title, date, description, url }));
 //fetching promises
 const fetchingPromises = [
     fetchHLEvents(),
@@ -23,11 +23,11 @@ const fetchingPromises = [
 ]
 //response from heel life and odum and adding to an array
 Promise.all(fetchingPromises).then(responses => {
-//response from heellife is responses[0]
-//response from odum is responses[1]
-//const allEvents = [heellife ones, odum ones, etc]
-//and sort by date
-//write to events.json file
+    //response from heellife is responses[0]
+    //response from odum is responses[1]
+    //const allEvents = [heellife ones, odum ones, etc]
+    //and sort by date
+    //write to events.json file
 })
-.catch()
-//changes 
+    .catch()
+//changes
