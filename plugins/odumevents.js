6f44913e1a242102648d2bd26885f26960f4617c
abunts <abunta@me.com>
2021-07-12 15:49:50 -0400

    uniform plugin syntax

--- plugins/odumevents.js
+++ plugins/odumevents.js
@@ -1,45 +1,33 @@
-//using the fetch api
+import fetch from "node-fetch";
 const api_url = 'https://odum.unc.edu/wp-json/tribe/events/v1/events';
 //setting up async function
 async function getAPI(api_url) {
     try {
-        //storing response
         const response = await fetch(api_url);
-        //storing data in json format
         var data = await response.json();
-        // console.log(data);
+        return data.value;
     }
     catch (error) {
         console.log(error);
     }
-    //accounting for json parsing error (enpoint might be sensitive to "user-agent")
-    headers: {
-        Accept: 'application/json, text/plain, */*',
-            'User-Agent';
-    }
-    return data.value;
+    return [];
 }
-//rendering event data from API
-async function renderAPI() {
+async function transformEvents(info) {
     let info = await getAPI(api_url);
-    let infoArray = await info.map(event => {
-        return { name: event.title, description: event.description, url: `https://odum.unc.edu/event/${event.slug}/${event.start_date}`, date: event.start_date }
-
-    });
-    var dataScienceEvents = await infoArray.filter((event) => {
-        return event.description.includes('practical frameworks'); //case sensitive
-    });
-    //writing data to JSON file
-    const fs = require('fs');
-    const jsonString = JSON.stringify(dataScienceEvents, null, 2);
-    fs.writeFile('./newEvents.json', jsonString, err => {
-        if (err) {
-            console.log('Error writing file', err)
-        } else {
-            console.log('Successfully wrote file')
-        }
-    });
-    console.log(dataScienceEvents);
+    return info.map(event => ({
+        name: event.title,
+        description: event.description,
+        url: `https://odum.unc.edu/event/${event.slug}/${event.start_date}`,
+        date: event.start_date
+    }));
 }
+var dataScienceEvents = await info.filter((event) => {
+    return event.description.includes('data science'); //case sensitive
+});
 
+export default async function () {
+    const info = await getAPI(api_url),
+    const transformedEvents = await transformEvents(info),
+    return transformedEvents
+};
 
