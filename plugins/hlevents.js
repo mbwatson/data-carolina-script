20966a63342d79e4a68c0564fd5dd2271b81010a
abunts <abunta@me.com>
2021-07-12 16:19:03 -0400

    nodemon start script update

--- plugins/hlevents.js
+++ plugins/hlevents.js
@@ -1,34 +1,36 @@
 import fetch from "node-fetch";
-//creating the api to call
+
 const api_url = 'https://heellife.unc.edu/api/discovery/event/search?endsAfter=2021-06-22T16%3A12%3A48-04%3A00&orderByField=endsOn&orderByDirection=ascending&status=Approved&take=15&query=';
+
 async function getAPI(api_url) {
     try {
         const response = await fetch(api_url);
         var data = await response.json();
         return data.value;
-    } 
+    }
     catch (error) {
         console.log(error);
     }
-   return [];
+    return [];
 }
 async function transformEvents(info) {
-    let info = await getAPI(api_url);
-     return info.map(event => ({
-        name: event.name, 
-        description: event.description, 
-        url: `https://heellife.unc.edu/event/${event.id}`, 
-        date: event.startsOn, }));
-     }
-    var dataScienceEvents = await info.filter((event) => {
+    info = await getAPI(api_url);
+    info.map(event => ({
+        name: event.name,
+        description: event.description,
+        url: `https://heellife.unc.edu/event/${event.id}`,
+        date: event.startsOn,
+    }));
+    info.filter((event) => {
         return event.description.includes('data science'); //case sensitive
-    });
+    })
+}
 
 
-export default async function(){
-    const info = await getAPI(api_url), 
-    const transformedEvents = await transformEvents(info), 
+export default async function () {
+    const info = await getAPI(api_url)
+    const transformedEvents = await transformEvents(info)
     return transformedEvents
-};
+}
 
 
--- plugins/odumevents.js
+++ plugins/odumevents.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
+
 const api_url = 'https://odum.unc.edu/wp-json/tribe/events/v1/events';
-//setting up async function
+
 async function getAPI(api_url) {
     try {
         const response = await fetch(api_url);
@@ -13,21 +14,22 @@ async function getAPI(api_url) {
     return [];
 }
 async function transformEvents(info) {
-    let info = await getAPI(api_url);
-    return info.map(event => ({
+    info = await getAPI(api_url);
+    info.map(event => ({
         name: event.title,
         description: event.description,
         url: `https://odum.unc.edu/event/${event.slug}/${event.start_date}`,
         date: event.start_date
     }));
+    info.filter((event) => {
+        return event.description.includes('data science'); //case sensitive
+    })
 }
-var dataScienceEvents = await info.filter((event) => {
-    return event.description.includes('data science'); //case sensitive
-});
+
 
 export default async function () {
-    const info = await getAPI(api_url),
-    const transformedEvents = await transformEvents(info),
+    const info = await getAPI(api_url)
+    const transformedEvents = await transformEvents(info)
     return transformedEvents
 };
 
