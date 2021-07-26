66c9fb0e7a34ee40dbfa12c479596fd292cc5025
abunts <abunta@me.com>
2021-07-26 14:51:02 -0400

    new plugin events added to API file

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,10 +1,17 @@
 const fetchHLEvents = require('../../plugins/hlevents.js');
 const fetchOdumEvents = require('../../plugins/odumevents.js');
+const fetchDSminorEvents = require('../../plugins/DSminorevents.js');
+const fetchGillingsEvents = require('../../plugins/gillingsevents.js');
+const fetchStatsEvents = require('../../plugins/statsevents.js');
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
   const odumEvents = await fetchOdumEvents();
-  return [...hlevents, ...odumEvents];
+  const dsEvents = await fetchDSminorEvents();
+  const gillingsEvents = await fetchGillingsEvents();
+  const statsEvents = await fetchStatsEvents();
+
+  return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
 
 export default async (req, res) => {
