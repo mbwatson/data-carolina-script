2d110dfeb22b5c7099d57225d70cc1955a53f0f7
abunts <abunta@me.com>
2021-07-28 14:52:00 -0400

    unc main website events plugin added

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,6 +3,9 @@ import fetchOdumEvents from '../../plugins/odumevents.js';
 import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
+import fetchUNCMainEvents from '../../plugins/uncevents.js';
+
+import { useRouter } from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
@@ -10,9 +13,18 @@ const fetchEvents = async () => {
   const dsEvents = await fetchDSminorEvents();
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
+  const uncMainEvents = await fetchUNCMainEvents();
 
-  return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
+  return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents, ...uncMainEvents];
 }
+
+const Post = () => {
+  const router = useRouter();
+  const { pid } = router.query
+
+  return <p>Post: {pid}</p>
+}
+export default Post
 
 export default async (req, res) => {
   const events = await fetchEvents()
