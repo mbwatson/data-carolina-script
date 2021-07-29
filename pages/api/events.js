a6b6ca6e2bcfccf2b233455d29c262fca7880052
abunts <abunta@me.com>
2021-07-29 10:50:32 -0400

    updates

--- pages/api/events.js
+++ pages/api/events.js
@@ -9,12 +9,10 @@ import fetchStatsEvents from '../../plugins/statsevents.js';
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
   const odumEvents = await fetchOdumEvents();
-  /*
   const dsEvents = await fetchDSminorEvents();
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
-*/
- return [...hlevents, ...odumEvents];
+ return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
 
 /*
@@ -26,6 +24,7 @@ const Post = () => {
 }
 export default Post
 */
+
 
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
