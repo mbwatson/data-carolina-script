88ebc99286f5f1f342a39b774575f0bbdaf26e98
Abby Bunta <67069243+abunts@users.noreply.github.com>
2021-07-29 11:07:21 -0400

    proxy testing (#7)
    * lambda build added
    
    * import function updated
    
    * unc main website events plugin added
    
    * import routing function
    
    * unc main events plugin removed
    
    * proxy added (testing it)
    
    * updates with routing
    
    * debugging
    
    * certificate verification updates
    
    * certificate verification updates
    
    * updates
    
    * updates

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,20 +3,32 @@ import fetchOdumEvents from '../../plugins/odumevents.js';
 import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
+//import useRouter from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
   const odumEvents = await fetchOdumEvents();
-  /*
   const dsEvents = await fetchDSminorEvents();
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
-*/
- return [...hlevents, ...odumEvents];
+
+ return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
+
+/*
+const Post = () => {
+  const router = useRouter();
+  const { slug } = router.query
+{slug : '/events/[.slug].js'}
+  return <p>Post: {slug}</p>
+  
+}
+export default Post
+*/
 
 
 export default async (req, res) => {
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
   const events = await fetchEvents()
   res.send(events)
 }
