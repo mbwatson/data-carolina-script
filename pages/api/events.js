c44431ed7109016946a83e744701fbed350d9851
abunts <abunta@me.com>
2021-07-30 16:09:37 -0400

    route updates

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,7 +3,8 @@ import fetchOdumEvents from '../../plugins/odumevents.js';
 import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
-import searchEvents from '../../api/events.js';
+import useRouter from 'next/router';
+import Router from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
@@ -15,9 +16,17 @@ const fetchEvents = async () => {
   return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
 
+const Index = () => {
+  const router = useRouter()
+  const {keyword} = router.query;
+  keyword = 'data science';
+  //return(<div>{keyword}</div>);
+  console.log(keyword);
+}
+
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
   const events = await fetchEvents();
-  events.searchEvents('data');
   res.send(events)
+  res.send(Index)
 }
