fb8037fb3a4888d1b04adc7777081813b3bada29
abunts <abunta@me.com>
2021-07-30 16:58:18 -0400

    filter testing

--- pages/api/events.js
+++ pages/api/events.js
@@ -4,7 +4,6 @@ import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
 import useRouter from 'next/router';
-import Router from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
@@ -12,21 +11,30 @@ const fetchEvents = async () => {
   const dsEvents = await fetchDSminorEvents();
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
-
+  
   return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
+  /*
+  let dataEvents = [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
+  var sortedEvents = await dataEvents.filter((event) => {
+    return event.title.includes('data science');
+  });
+  console.log(sortedEvents);
+  */
 }
-
+/*
 const Index = () => {
   const router = useRouter()
-  const {keyword} = router.query;
+  const { keyword } = router.query;
   keyword = 'data science';
   //return(<div>{keyword}</div>);
   console.log(keyword);
 }
-
+*/
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
   const events = await fetchEvents();
-  res.send(events)
-  res.send(Index)
+  let sortedEvents = await events.filter((event) => 
+    event.title.includes('Pride'));
+  res.send(sortedEvents)
+  //res.send(Index)
 }
