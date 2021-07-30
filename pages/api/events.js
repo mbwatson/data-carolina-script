334b56943ca4d8982dc46efb6ed5682d1d22eee9
abunts <abunta@me.com>
2021-07-30 13:03:59 -0400

    revert back to previous code

--- pages/api/events.js
+++ pages/api/events.js
@@ -11,16 +11,11 @@ const fetchEvents = async () => {
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
 
-  [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
-}
-async function filteredEvents() {
-  let dEvents = await fetchEvents();
-  var filterEvents = await dEvents.filter(event => (event.description.includes('pride')));
-  return filterEvents;
+  return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
 
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
-  const events = await filteredEvents();
+  const events = await fetchEvents();
   res.send(events)
 }
