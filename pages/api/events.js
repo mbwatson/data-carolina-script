2949899f354e827a697dd72e782a8a29ebc285e8
abunts <abunta@me.com>
2021-08-03 15:58:24 -0400

    route testing

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,7 +3,7 @@ import fetchOdumEvents from '../../plugins/odumevents.js';
 import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
-import useRouter from 'next/router';
+import router from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
@@ -11,29 +11,20 @@ const fetchEvents = async () => {
   const dsEvents = await fetchDSminorEvents();
   const gillingsEvents = await fetchGillingsEvents();
   const statsEvents = await fetchStatsEvents();
-  
+
   return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
-  /*
-  let dataEvents = [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
-  var sortedEvents = await dataEvents.filter((event) => {
-    return event.title.includes('data science');
-  });
-  console.log(sortedEvents);
-  */
 }
 
-const Index = () => {
-  const router = useRouter()
-  const { keyword } = router.query;
-  keyword = 'data science';
-  //return(<div>{keyword}</div>);
-  console.log(keyword);
+const useRouter = async () => {
+  app.get('https://uncdataevents.netlify.app/api/events', function (req, res){
+   router.post('/data+science');
+   res.send('data science events');
+  })
 }
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
   const events = await fetchEvents();
-  /*let sortedEvents = await events.filter((event) => 
-    event.title.includes('Pride'));
-  res.send(sortedEvents) */
-  res.send(Index)
+  const routing = await useRouter();
+  res.send(routing)
+  res.send(events)
 }
