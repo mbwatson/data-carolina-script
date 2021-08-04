d0da815d7d765f4c83cefa5d0c2dc9d5a05836ba
abunts <abunta@me.com>
2021-08-04 16:31:10 -0400

    data science filter events

--- pages/api/events.js
+++ pages/api/events.js
@@ -3,7 +3,6 @@ import fetchOdumEvents from '../../plugins/odumevents.js';
 import fetchDSminorEvents from '../../plugins/DSminorevents.js';
 import fetchGillingsEvents from '../../plugins/gillingsevents.js';
 import fetchStatsEvents from '../../plugins/statsevents.js';
-import router from 'next/router';
 
 const fetchEvents = async () => {
   const hlevents = await fetchHLEvents();
@@ -15,16 +14,11 @@ const fetchEvents = async () => {
   return [...hlevents, ...odumEvents, ...dsEvents, ...gillingsEvents, ...statsEvents];
 }
 
-const useRouter = async () => {
-  router.get('https://uncdataevents.netlify.app/api/events', function (req, res){
-   router.post('/data+science');
-   res.send('data science events');
-  })
-}
 export default async (req, res) => {
-  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //temporary fix... info can be edited by client (disables certificate verification)
-  const events = await fetchEvents();
-  const routing = await useRouter();
-  res.send(routing)
-  res.send(events)
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+  const pid = req.query.pid
+  const events = await fetchEvents()
+ .then(events => (events.filter(event => { return event.name.includes('Data Science')}
+ )));
+ res.send(events)
 }
