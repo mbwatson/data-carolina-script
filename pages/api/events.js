e3c8ac38774e3405f161719ebe9aebb4cd9528a0
abunts <abunta@me.com>
2021-08-26 16:45:29 -0400

    filter testing

--- pages/api/events.js
+++ pages/api/events.js
@@ -17,7 +17,8 @@ const fetchEvents = async () => {
 export default async (req, res) => {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
   const pid = req.query.pid
-  const events = await fetchEvents()
-    .then (events => {return events.filter(event => events.name.includes(pid))});
-  res.send(events)
+  if(pid.includes("Analytics")){
+    const events = await fetchEvents()
+    .then (events => {return events.filter(event => event.name.includes(pid)|| event.description.includes(pid))});
+  res.send(events)}
 }
