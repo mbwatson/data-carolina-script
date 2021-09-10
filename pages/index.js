5b1350e281be719457d515a638774f2d0573bfbc
Matt Watson <mattwat@gmail.com>
2021-09-10 16:35:35 -0400

    store events in application state

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Home() {
+  // variable to store the events in front-end's "state"
+  // `events` contains the event data
+  // `setEvents` is a function to assign the `events` variable
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
     const fetchEvents = async () => {
       const response = await axios.get('/api/events');
-      if(!response.data){
+      if (!response.data) {
         return [];
       }
-      console.log(response)
+      // here, we're using the `setEvents` function to, ...well, set events
+      // with the value of the `data` property in the response object.
+      setEvents(response.data);
     }
-    fetchEvents()
-  }, [])
+    fetchEvents();
+  }, []);
   
   return (
     <div>
