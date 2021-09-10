4646f1ee0ea2b66ae1ad1fa3d488949cbe3a31d2
Matt Watson <mattwat@gmail.com>
2021-09-10 16:51:50 -0400

    add individual event styling

--- components/events-list.js
+++ components/events-list.js
@@ -6,7 +6,7 @@ export const EventsList = ({ events }) => {
       {
         events.map((event, i) => {
           return (
-            <div key={ i }>
+            <div key={ i } className={ styles.event }>
               <h3>{ event.name }</h3>tig
               <p>{ event.description }</p>
             </div>
